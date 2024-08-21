c5162785e3fff081e8bb1e19f38f112e9df775ad
Bhuvi1108 <115173541+Bhuvi1108@users.noreply.github.com>
2024-08-21 23:09:39 +0530

    Add files via upload

---
+++ contactbook/contactoutput.py
@@ -0,0 +1,73 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Rakshalotus/AppData/Local/Programs/Python/Python312/contact book.py
+Contact Management System
+1. Add Contact
+2. View Contact List
+3. Search Contact
+4. Update Contact
+5. Delete Contact
+6. Exit
+
+Enter your choice: 1
+Enter store name: pandiyan store
+Enter phone number: 9876543569
+Enter email: abc123@gmail.com
+Enter address: no 10, anna nager,chennai
+
+Contact 'pandiyan store' added successfully.
+
+Contact Management System
+1. Add Contact
+2. View Contact List
+3. Search Contact
+4. Update Contact
+5. Delete Contact
+6. Exit
+
+Enter your choice: 1
+Enter store name: fancy store
+Enter phone number: 8976543289
+Enter email: fancy123@gmail.com
+Enter address:  no 20,anna nager,chennai
+
+Contact 'fancy store' added successfully.
+
+Contact Management System
+1. Add Contact
+2. View Contact List
+3. Search Contact
+4. Update Contact
+5. Delete Contact
+6. Exit
+
+Enter your choice: 1
+Enter store name: saravana store
+Enter phone number: 8976554998
+Enter email: saravana456@gmail.com
+Enter address: no37,anna nager chennai
+
+Contact 'saravana store' added successfully.
+
+Contact Management System
+1. Add Contact
+2. View Contact List
+3. Search Contact
+4. Update Contact
+5. Delete Contact
+6. Exit
+
+Enter your choice: 
+=================================================== RESTART: C:\Users\Rakshalotus\AppData\Local\Programs\Python\Python312\todolist.py ==================================================
+
+To-Do List Menu
+1. View To-Do List
+2. Add Task
+3. Update Task
+4. Delete Task
+5. Exit
+Enter your choice: 
+================================================== RESTART: C:\Users\Rakshalotus\AppData\Local\Programs\Python\Python312\calculator.py =================================================
+Enter the first number: 
+=================================================== RESTART: C:\Users\Rakshalotus\AppData\Local\Programs\Python\Python312\password.py ==================================================
