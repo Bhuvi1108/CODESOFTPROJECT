474d13d1aa77f5c0b6b0e919bcdb8c546f3ff3ce
Bhuvi1108 <115173541+Bhuvi1108@users.noreply.github.com>
2024-08-21 22:56:17 +0530

    Add files via upload

---
+++ todolist/todolistoutput.py
@@ -0,0 +1,38 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Users/Rakshalotus/Desktop/todolist.py ==============
+
+To-Do List Menu
+1. View To-Do List
+2. Add Task
+3. Update Task
+4. Delete Task
+5. Exit
+Enter your choice: 3
+
+To-Do List:
+No tasks available.
+Enter the task number to update: 23
+Invalid task number!
+
+To-Do List Menu
+1. View To-Do List
+2. Add Task
+3. Update Task
+4. Delete Task
+5. Exit
+Enter your choice: 2
+Enter a new task: go to school
+Task added!
+
+To-Do List Menu
+1. View To-Do List
+2. Add Task
+3. Update Task
+4. Delete Task
+5. Exit
+Enter your choice: 3
+
+To-Do List:
+1. go to school
